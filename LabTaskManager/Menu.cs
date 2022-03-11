7d62d40ceb175cfa22e0697394b5befd300ffc59
Allison Anguiano <aanguiano@dev-10.com>
2022-03-11 14:34:23 -0600

    More mess

--- LabTaskManager/Menu.cs
+++ LabTaskManager/Menu.cs
@@ -14,7 +14,7 @@ namespace LabTaskManager
         private const string quit = "Q";
         //switch
         //string[] taskList = new string[5];        confusion, maybe use loop to update null values
-        string[] taskList = { "1. Dishes", "2. Bills", "3. Groceries", "4. Vaccuum" };  //interpolation and indexing
+        string[] taskList = { "Dishes", "Bills", "Groceries", "Vaccuum", "" };  //interpolation and indexing
         //array of tasks, index or foreach
 
         public void Run()
@@ -31,7 +31,7 @@ namespace LabTaskManager
                         ViewList();
                         break;
                     case addTask:
-                        AddTask();
+                        AddTask(taskList);      //hmmmm [0]
                         break;
                     case removeTask:
                         RemoveTask();
@@ -56,7 +56,10 @@ namespace LabTaskManager
             //need a loop here on what to write on list... missing something I feel, oh null parts of array
             for (int i = 0; i < taskList.Length; i++)
             {
-                Console.WriteLine($"{taskList[i]}");
+                if (!string.IsNullOrEmpty(taskList[i]))
+                {
+                    Console.WriteLine($"{i + 1}. {taskList[i]}");
+                }    
             }
             Console.WriteLine("-------------");
             Console.Write("Press any key to continue ");
@@ -64,24 +67,48 @@ namespace LabTaskManager
             Console.Clear();
         }
 
-        private void AddTask()
+        private string[] AddTask(string[] currentTasks)
         {
+            //cosider moving the asking for a task into another method
             Console.Clear();
             Console.WriteLine("Enter Task: ");
             string newTask = Console.ReadLine();
+
 
             while (string.IsNullOrEmpty(newTask))
             {
                 Console.WriteLine("Enter Task: ");
                 newTask = Console.ReadLine();
             }
-            //add to array
+
+            for (int i = 0; i < currentTasks.Length; i++)
+            {
+                if (string.IsNullOrEmpty(currentTasks[i]))
+                { 
+                    currentTasks[i] = newTask; 
+                }
+                //else
+                //{
+                //    //double array size if array is full, use IsNull
+                //    //Copy values from old array into new array
+                //    string[] newArray = new string[currentTasks.Length *2];
+                //    for (int ix = 0; ix < currentTasks.Length; ix++)
+                //    {
+                //        newArray[ix] = currentTasks[ix];
+                //    }
+                //    //Set old array variable to point to new array
+                //    currentTasks = newArray;
+                //    currentTasks[i] = newTask;
+                //}
+            }
+
+            
 
             Console.WriteLine("It's on the list!");
             Console.Write("Press any key to continue ");
             Console.ReadKey();
             Console.Clear();
-            //return array
+            return currentTasks;
         }
 
         private void RemoveTask()
@@ -106,7 +133,7 @@ namespace LabTaskManager
 
         private void HandleUnknownInput()
         {
-            Console.WriteLine("Wrong");         //adapt to request valid input
+            Console.WriteLine("Wrong");         //adapt to request valid input, maybe not needed right now
         }
 
         private string BuildMenu()
